aa79f91ef8dff864706a9be8d4b095309e8609bb
dshaps10 <dan.shapiro1210@gmail.com>
2017-06-21 11:10:55 -0400

    Add 'hasVotedFor()' function

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -9,14 +9,21 @@ export default class Voting extends Component {
     return !!this.props.hasVoted;
   }
 
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }
+
   render() {
     return (
       <div className="voting">
         {this.getPair().map(entry =>
           <button
             key={entry}
+            disabled={this.isDisabled()}
             onClick={() => this.props.vote(entry)}>
             <h1>{entry}</h1>
+            {this.hasVotedFor(entry) ?
+              <div className="label">Voted</div> : null}
           </button>
         )}
       </div>
