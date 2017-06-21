aea13f5b1ce0818ed4a1e86fa8c45c5ff955d6bd
dshaps10 <dan.shapiro1210@gmail.com>
2017-06-21 11:07:30 -0400

    Add specs and 'hasVoted()' function

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 
-class Voting extends Component {
+export default class Voting extends Component {
   getPair() {
     return this.props.pair || [];
   }
-  
+
+  isDisabled() {
+    return !!this.props.hasVoted;
+  }
+
   render() {
     return (
       <div className="voting">
         {this.getPair().map(entry =>
-          <button key={entry}>
+          <button
+            key={entry}
+            onClick={() => this.props.vote(entry)}>
             <h1>{entry}</h1>
           </button>
         )}
