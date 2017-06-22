de15d8e624406422a94b45ee090ef1f56d636900
dshaps10 <dan.shapiro1210@gmail.com>
2017-06-22 08:55:07 -0400

    Finish refactoring 'Voting' component

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -1,18 +1,8 @@
 import React, {Component} from 'react';
+import Winner from './Winner';
+import Vote from './Vote';
 
 export default class Voting extends Component {
-  getPair() {
-    return this.props.pair || [];
-  }
-
-  isDisabled() {
-    return !!this.props.hasVoted;
-  }
-
-  hasVotedFor(entry) {
-    return this.props.hasVoted === entry;
-  }
-
   render() {
     return (
       <div>
